e5905fbe923f2ce99d12471f91eb2b5cbffd3b76
Liam Vermette <Liam.vermette@gmail.com>
2024-03-03 21:14:48 -0500

    still going to add background in

--- move.js
+++ move.js
@@ -7,7 +7,7 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
     
-    function moveWithArrowKeys(left, bottom){
+    function moveWithArrowKeys(left, bottom, callback){
         let direction = null;
         let x = left;
         let y = bottom;
@@ -49,10 +49,12 @@ function move(element) {
             if(e.key === 'ArrowDown'){
                 direction = 'south'
             }
+            callback(direction)
         })
         
         document.addEventListener('keyup', function(e){
             direction = null
+            callback(direction)
         })
     }
     
